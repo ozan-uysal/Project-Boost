2bcdbb35aabdbef272e90b4aaa5d0b8feec5315a
OyunGelistirici <uysalozan92@gmail.com>
2022-12-05 17:17:02 +0300

    2

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,23 +1,28 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
         {
             case "Friendly":
-
+                break;
             case "finish":
-
+                break;
             case "Fuel":
-
-
-
+                break;
             default:
+                ReloadLevel();
                 break;
         }
 
+    }
+    public void ReloadLevel()
+    {
+        int currentScene=SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
